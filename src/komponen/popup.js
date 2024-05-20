9b39335474528922dc1bc71d47d9c3bcbecf7445
zakarial0 <dedezakariyal017@gmail.com>
2024-05-20 20:44:32 +0700

    test netlify

--- src/komponen/popup.js
+++ src/komponen/popup.js
@@ -4,7 +4,8 @@ const Popup = () => {
     return(
         <div>
             <h1 className=" text-blue-500 text-2xl">Work In Progres</h1>
-            <p className=" text-red-600 ">sgsfdh</p>
+            <h4>ERRor </h4>
+            <p className=" text-red-600 text-2xl">tailwind doesnt work, i dont know why :l</p>
         </div>
     )
 }
